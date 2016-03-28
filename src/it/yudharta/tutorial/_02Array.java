9aff96badb5e9a7fd1fd015eaca31d92f4cce6c7
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-03-28 11:48:56 +0700

    array

---
+++ src/it/yudharta/tutorial/_02Array.java
@@ -0,0 +1,14 @@
+package it.yudharta.tutorial;
+
+public class _02Array {
+    public static void main(String[] args) {
+        int[] nomer = {1,2,3,4,5,6,7,8,9};
+        int[][] kotak = {{1,2,3},{4,5,6}};
+        
+        nomer = new int[4];
+        nomer[0] = 7;
+        nomer[1] = 8;
+        
+        System.out.println("nomer[0] =" + nomer[0]);
+    }
+}
